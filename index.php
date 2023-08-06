0ee59811c96aa2ee70a4d152b2f57fc7c7f2a276
hagaikestest <141217192+hagaikestest@users.noreply.github.com>
2023-08-06 13:29:15 +0300

    back

--- index.php
+++ index.php
@@ -12,33 +12,6 @@ if( $_POST['_upl'] == "Upload task document" ) {
 if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Your document upload successfully!<b><br><br>'; }
 else { echo '<b>file not uploaded!</b><br><br>'; }
 }
-
-$url = $_GET['url'];
-
-// Function to fetch content from the specified URL
-function fetchContent($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    return $content;
-}
-
-// Process the user-provided URL
-if (isset($url)) {
-    // Check if the URL starts with 'http://' or 'https://'
-    if (strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0) {
-        $url = 'http://' . $url; // Assume http:// if no scheme is provided
-    }
-
-    $content = fetchContent($url);
-
-    // Display the fetched content
-    echo "<pre>" . htmlentities($content) . "</pre>";
-}
-
-
 ?>
   
   <body>
@@ -55,13 +28,8 @@ if (isset($url)) {
         echo "<pre>$output</pre>";
       }
     ?>
-
-<h1>SSRF Lab</h1>
-    <form action="" method="GET">
-        <label for="url">Enter URL:</label>
-        <input type="text" name="url" id="url">
-        <input type="submit" value="Fetch URL">
-    </form>
   </body>
 </html>
+
+
 
